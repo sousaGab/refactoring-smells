267504bf78f0fa2b31e7d300f3cce24baf1a4965
sousaGab <gabrielamaralsousa@gmail.com>
2025-05-08 06:15:48 -0300

    refactored intl-te-input project resuts

---
+++ refactoring_data/copilot/smell_61/original_test.js
@@ -0,0 +1,199 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const intlTelInput = require("intl-tel-input");
+const { initPlugin, resetPackageAfterEach } = require("../helpers/helpers");
+require("../helpers/matchers");
+
+describe("attachUtils", function() {
+  resetPackageAfterEach(intlTelInput);
+
+  describe("calling attachUtils before init plugin", () => {
+
+    const utilsLoader = () => import("intl-tel-input/utils");
+    let loadResult;
+
+    beforeEach(() => {
+      loadResult = intlTelInput.attachUtils(utilsLoader);
+    });
+
+    it("starts loading the utils", () => {
+      expect(intlTelInput).toHaveProperty("startedLoadingUtilsScript", true);
+    });
+
+    it("resolves the promise", async () => {
+      expect(loadResult).toBeAPromise();
+      await expect(loadResult).resolves.toBe(true);
+    });
+
+    it("installs the utils module at intlTelInput.utils", async () => {
+      await loadResult;
+
+      expect(intlTelInput).toHaveProperty("utils.isValidNumber");
+    });
+
+    describe("then init plugin with loadUtils option", () => {
+
+      it("resolves the instance's promise", async () => {
+        const { iti } = initPlugin({
+          intlTelInput,
+          options: {
+            loadUtils: () => import("some/other/url/ok"),
+          },
+        });
+        await iti.promise;
+      });
+
+    });
+
+  });
+
+
+
+  describe("init plugin with loadUtils option, but force documentReady=false so it wont fire", function() {
+    /** @type {jest.Mock<() => Promise<any>>} */
+    let utilsLoader;
+    /** @type {intlTelInput.Iti} */
+    let iti;
+
+    beforeEach(function() {
+      jest.spyOn(intlTelInput, "documentReady").mockReturnValue(false);
+      utilsLoader = jest.fn(async () => import("intl-tel-input/utils"));
+
+      ({ iti } = initPlugin({
+        intlTelInput,
+        options: { loadUtils: utilsLoader },
+      }));
+    });
+
+    it("does not start loading the utils", function() {
+      expect(intlTelInput).toHaveProperty("startedLoadingUtilsScript", false);
+    });
+
+    it("does not resolve the promise", async function() {
+      await expect(iti.promise).toBePending();
+    });
+
+
+
+    describe("calling attachUtils", function() {
+      /** @type {Promise<any>} */
+      let attachUtilsPromise;
+
+      beforeEach(async function() {
+        attachUtilsPromise = intlTelInput.attachUtils(utilsLoader);
+      });
+
+      it("starts loading the utils", function() {
+        expect(intlTelInput).toHaveProperty("startedLoadingUtilsScript", true);
+      });
+
+      it("resolves the promise", async function() {
+        await expect(attachUtilsPromise).resolves.toBe(true);
+      });
+
+
+
+      describe("then init another plugin instance with loadUtils option", function() {
+
+        beforeEach(async function() {
+          // Wait for previous load to finish.
+          await attachUtilsPromise;
+
+          initPlugin({
+            intlTelInput,
+            options: { loadUtils: utilsLoader },
+          });
+        });
+
+        it("only loads once", function() {
+          expect(utilsLoader).toHaveBeenCalledTimes(1);
+        });
+
+      });
+
+    });
+
+  });
+
+
+
+  describe("force documentReady=true then init plugin with loadUtils", function() {
+
+    const utilsLoader = () => import("intl-tel-input/utils");
+    /** @type {intlTelInput.Iti} */
+    let iti;
+
+    beforeEach(function() {
+      jest.spyOn(intlTelInput, "documentReady").mockReturnValue(true);
+      ({ iti } = initPlugin({
+        intlTelInput,
+        options: { loadUtils: utilsLoader },
+      }));
+    });
+
+    it("resolves the promise immediately", async function() {
+      await expect(iti.promise).resolves.toBeInstanceOf(Array);
+    });
+
+    it("starts loading the utils", function() {
+      expect(intlTelInput).toHaveProperty("startedLoadingUtilsScript", true);
+    });
+
+  });
+
+
+
+  describe("calling with a function", function() {
+    const mockUtils = { default: { mymodule: "fakeutils" } };
+
+    it("uses the object the function resolves with", async () => {
+      const result = await intlTelInput.attachUtils(async () => mockUtils);
+
+      expect(result).toEqual(true);
+      expect(intlTelInput.utils).toBe(mockUtils.default);
+    });
+
+    it("rejects if the function rejects", async () => {
+      const loadPromise = intlTelInput.attachUtils(async () => {
+        throw new Error("Uhoh!");
+      });
+
+      await expect(loadPromise).rejects.toThrow("Uhoh!");
+    });
+
+    it("rejects if the function throws", async () => {
+      const loadPromise = intlTelInput.attachUtils(() => {
+        throw new Error("Uhoh!");
+      });
+
+      await expect(loadPromise).rejects.toThrow("Uhoh!");
+    });
+
+    it("rejects if the function returns a non-promise", async () => {
+      const loadPromise = intlTelInput.attachUtils(() => ({
+        anObject: "That is not a promise",
+      }));
+
+      await expect(loadPromise).rejects.toThrow();
+    });
+
+    it("rejects if the function resolves to a non-object", async () => {
+      const loadPromise = intlTelInput.attachUtils(async () => "Hello!");
+
+      await expect(loadPromise).rejects.toThrow();
+    });
+
+    it("does not call the function a second time", async () => {
+      const loader = jest.fn(async () => mockUtils);
+
+      await intlTelInput.attachUtils(loader);
+      await intlTelInput.attachUtils(loader);
+
+      expect(loader).toHaveBeenCalledTimes(1);
+    });
+
+  });
+
+});
---
+++ refactoring_data/copilot/smell_65/refactored_test.js
@@ -0,0 +1,73 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { userEvent } = require("@testing-library/user-event");
+const {
+  initPlugin,
+  teardown,
+  getCountryListLength,
+  checkFlagSelected,
+  openDropdownSelectCountryAsync,
+  clickSelectedCountryAsync,
+  isDropdownOpen,
+} = require("../helpers/helpers");
+
+let user, iti1, iti2, input1, container1, container2;
+
+describe("multiple instances", () => {
+  beforeEach(() => {
+    user = userEvent.setup();
+    ({ iti: iti1, input: input1, container: container1 } = initPlugin({
+      options: { onlyCountries: ["af", "cn"] },
+    }));
+    ({ iti: iti2, container: container2 } = initPlugin({
+      options: { onlyCountries: ["al", "cn", "kr", "ru"] },
+    }));
+  });
+      
+  afterEach(() => {
+    teardown(iti1);
+    teardown(iti2);
+  });
+  
+  test("instances have different country lists", () => {
+    const expectedCountryListLengthInstance1 = 2;
+    const expectedCountryListLengthInstance2 = 4;
+
+    expect(getCountryListLength(container1)).toEqual(expectedCountryListLengthInstance1);
+    expect(getCountryListLength(container2)).toEqual(expectedCountryListLengthInstance2);
+  });
+
+  test("selecting a country in the first instance dropdown only updates the selected country for that instance", async () => {
+    await openDropdownSelectCountryAsync(container1, "cn", user);
+
+    expect(checkFlagSelected(container1, "cn")).toBe(true);
+    expect(checkFlagSelected(container2, "")).toBe(true);
+  });
+
+  test("typing an intl number in the first instance input only updates the selected country for that instance", async () => {
+    await user.type(input1, "+86123456");
+
+    expect(checkFlagSelected(container1, "cn")).toBe(true);
+    expect(checkFlagSelected(container2, "")).toBe(true);
+  });
+
+  describe("clicking open dropdown on the first instance", () => {
+    beforeEach(async () => {
+      await clickSelectedCountryAsync(container1, user);
+    });
+
+    test("only opens the dropdown on that instance", () => {
+      expect(isDropdownOpen(container1)).toBe(true);
+      expect(isDropdownOpen(container2)).toBe(false);
+    });
+
+    test("then clicking open dropdown on the second instance will close the first and open the second", async () => {
+      await clickSelectedCountryAsync(container2, user);
+
+      expect(isDropdownOpen(container1)).toBe(false);
+      expect(isDropdownOpen(container2)).toBe(true);
+    });
+  });
+});
